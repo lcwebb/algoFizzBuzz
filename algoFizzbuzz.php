72c1ef121c5e3b5f10a3f6dd9a5dc0fdb5db864f
Ludovic Chovino <ludovicchovino@MacBook-Air-de-Ludovic.local>
2024-01-22 11:46:46 +0100

    test fizzbuzz php

---
+++ algoFizzbuzz.php
@@ -0,0 +1,19 @@
+<?php
+
+function algoFizzBuzz($n) {
+    for ($i = 1; $i <= $n; $i++) {
+        if ($i % 3 === 0 && $i % 5 === 0) {
+            echo ' FizzBuzz ';
+        } elseif ($i % 3 === 0) {
+            echo ' Fizz ';
+        } elseif ($i % 5 === 0) {
+            echo ' Buzz ';
+        } else {
+            echo $i.' ';
+        }
+    }
+}
+
+algoFizzBuzz(23);
+
+?>
